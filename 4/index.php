73159f011bfd798c6456a64e7f2250c5ef3f4c3f
StereoZing <94567076+StereoZing@users.noreply.github.com>
2025-03-31 08:24:30 +0300

    Update index.php

--- 4/index.php
+++ 4/index.php
@@ -1,1 +1,192 @@
+<?php
+header('Content-Type: text/html; charset=UTF-8');
 
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+	
+$error = FALSE;
+
+if (strlen($_POST['name1'])>150) {
+	setcookie('name1_error', 'ФИО слишком длинное.');
+	$error = TRUE;
+} elseif (preg_match('~[0-9]+~', $_POST['name1'])) {
+	setcookie('name1_error', 'ФИО не должно содержать цифры.');
+	$error = TRUE;
+} else {
+	setcookie('name1_error', '', strtotime("-1 day"));
+}
+setcookie('name1_value', $_POST['name1'], strtotime('+1 year'));
+
+if (!preg_match('/^[0-9]{10}$/', $_POST['tel'])) {
+	setcookie('tel_error', 'Номер должен содержать ровно 10 цифр.');
+	$error = TRUE;
+} else {
+	setcookie('tel_error', '', strtotime("-1 day"));
+}
+setcookie('tel_value', $_POST['tel'], strtotime('+1 year'));
+
+if (!preg_match('~@~', $_POST['email'])) {
+	setcookie('email_error', 'Email должен содержать \'@\'.');
+	$error = TRUE;
+} else {
+	setcookie('email_error', '', strtotime("-1 day"));
+}
+setcookie('email_value', $_POST['email'], strtotime('+1 year'));
+
+$year =(int)substr($_POST['date1'],0,4);
+if($year<1800){
+	setcookie('date1_error', 'Вы долгожитель?');
+	$error = TRUE;
+} elseif ($year>2025) {
+	setcookie('date1_error', 'Вы из будущего?');
+	$error = TRUE;
+} else {
+	setcookie('date1_error', '', strtotime("-1 day"));
+}
+setcookie('date1_value', $_POST['date1'], strtotime('+1 year'));
+
+if (empty($_POST['languages'])){
+	setcookie('languages_error', 'Выберите хотя бы JavaScript');
+	setcookie('languages_value', '', strtotime('-1 day'));
+	$error = TRUE;
+} else {
+	setcookie('languages_error', '', strtotime("-1 day"));
+	setcookie('languages_value', implode('|', ($_POST['languages'])), strtotime('+1 year'));
+}
+
+if (!preg_match('/^[0, 1]$/', $_POST['pol'])){
+	setcookie('pol_error',"Ты как это сделал?");
+	$error=TRUE;
+} else {
+	setcookie('pol_error', '', strtotime("-1 day"));
+}
+setcookie('pol_value', $_POST['pol'], strtotime('+1 year'));
+
+if (strlen($_POST['biography'])>200){
+	setcookie('biography_error', 'Ваша биография трогает душу...<br/>Но уменьшите её (душу)');
+	$error = TRUE;
+} else {
+	setcookie('biography_error', '', strtotime("-1 day"));
+}
+setcookie('biography_value', $_POST['biography'], strtotime('+1 year'));
+
+if ($error) {
+	header('Location: index.php');
+	exit();
+}
+
+include("../../../pass.php");
+$db = new PDO("mysql:host=localhost;dbname=$dbname", $user, $pass,
+  [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+
+try {
+	$stmt = $db->prepare("INSERT INTO applications VALUES (0, ?, ?, ?, ?, ?, ?)");
+	$stmt -> execute([$_POST['name1'], $_POST['tel'], $_POST['email'],
+					$_POST['date1'], $_POST['pol'], $_POST['biography']]);
+	$id_app = $db -> lastInsertId();
+	foreach($_POST['languages'] as $value) {
+		$stmt = $db->prepare("INSERT INTO app_languagess VALUES (?, ?)");
+		$stmt -> execute([$id_app,$value]);
+	}
+}
+catch(PDOException $e){
+	setcookie('DBerror', 'Error : ' . $e->getMessage());
+	header('Location: index.php');
+	exit();
+}
+
+setcookie('save', 'Успешно');
+header('Location: index.php');
+exit();
+}
+
+?>
+
+<!DOCTYPE html>
+
+<html languages="ru">
+
+  <head>
+    <meta charset="utf-8">
+    <title>Лабороторная работа №4</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+      integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmdate15pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+      rel="stylesheet" crossorigin="anonymous" />
+    <link rel="stylesheet" href="styleH.css" />
+  </head>
+
+  <body class="fs-120p ff-sans-serif" id="body">
+
+    <main class="mxw-960 container g-0 d-flex flex-column justify-content-center">
+
+      <section class="content order-3 order-sm-3 mt-3 p-3" id="form">
+        <h2 class="text-center fw-bold">Форма</h2>
+
+        <form action="./" method="POST">
+            <label>
+                Имя:<br />
+                <input name="name1" required />
+            </label><br />
+
+            <label>
+                Телефон:<br />
+                <input type="tel" name="tel" required />
+            </label><br />
+
+            <label>
+                Email:<br />
+                <input type="email" name="email" required />
+            </label><br />
+
+            <label>
+                Дата рождения:<br />
+                <input type="date" name="date1" />
+            </label><br />
+
+            Пол:<br />
+            <label>
+                <input type="radio" checked="checked" name="pol"
+                       value="0" /> Муж
+            </label>
+            <label>
+                <input type="radio" name="pol" value="1" /> Жен
+            </label><br />
+
+            <label>
+                Любимые языки программирования:<br />
+                <select name="languagesuages[]" multiple="multiple">
+                    <option value="1">Pascal</option>
+                    <option value="2">C</option>
+                    <option value="3">C++</option>
+                    <option value="4">JavaScript</option>
+                    <option value="5">PHP</option>
+                    <option value="6">Python</option>
+                    <option value="7">Java</option>
+                    <option value="8">Haskel</option>
+                    <option value="9">Clojure</option>
+                    <option value="10">Prolog</option>
+                    <option value="11">Scala</option>
+                </select>
+            </label><br />
+
+            <label>
+                Биография:<br />
+                <textarea name="biographygraphy"></textarea>
+            </label><br />
+
+            <label>
+                <input type="checkbox" name="familiarized" required />
+                Ознакомлен с контрактом
+            </label><br />
+
+            <button type="submit">Сохранить</button>
+        </form>
+      </section>
+    </main>
+
+  </body>
+
+</html>
