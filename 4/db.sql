8eb48b25b552cd853187e5973f9de0c1859fb277
StereoZing <94567076+StereoZing@users.noreply.github.com>
2025-05-05 08:59:46 +0300

    Add files via upload

---
+++ 4/db.sql
@@ -0,0 +1,35 @@
+﻿CREATE TABLE applications (
+  id_app int(10) unsigned NOT NULL AUTO_INCREMENT,
+  name1 varchar(150) NOT NULL,
+  tel bigint(10) NOT NULL,
+  email varchar(80) NOT NULL,
+  date1 DATE NOT NULL,
+  pol int(1) NOT NULL,
+  biography varchar(200),
+  PRIMARY KEY (id_app)
+) ENGINE = InnoDB;
+
+CREATE TABLE languages (
+	id_lang int(2) unsigned NOT NULL AUTO_INCREMENT,
+	lang varchar(10) NOT NULL,
+	PRIMARY KEY (id_lang)
+) ENGINE = InnoDB;
+
+INSERT INTO languages VALUES(0, 'Pascal');
+INSERT INTO languages VALUES(0, 'C');
+INSERT INTO languages VALUES(0, 'C++');
+INSERT INTO languages VALUES(0, 'JavaScript');
+INSERT INTO languages VALUES(0, 'PHP');
+INSERT INTO languages VALUES(0, 'Python');
+INSERT INTO languages VALUES(0, 'Java');
+INSERT INTO languages VALUES(0, 'Haskel');
+INSERT INTO languages VALUES(0, 'Clojure');
+INSERT INTO languages VALUES(0, 'Prolog');
+INSERT INTO languages VALUES(0, 'Scala');
+
+CREATE TABLE app_langs (
+	id_app int(10) unsigned NOT NULL,
+	id_lang int(2) unsigned NOT NULL,
+	FOREIGN KEY (id_app) REFERENCES applications (id_app),
+	FOREIGN KEY (id_lang) REFERENCES languages (id_lang)
+) ENGINE = InnoDB;
